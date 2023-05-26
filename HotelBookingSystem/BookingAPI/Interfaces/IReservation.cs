1f0786c6f89504b4c462d4ccae5344f62ade10d5
Mathanraj T <madhanmani0601@gmail.com>
2023-05-26 12:58:52 +0530

    Added Interfaces For BookingAPI

---
+++ HotelBookingSystem/BookingAPI/Interfaces/IReservation.cs
@@ -0,0 +1,13 @@
+﻿using BookingAPI.Models;
+
+namespace BookingAPI.Interfaces
+{
+    public interface IReservation
+    {
+        ICollection<Reservation> GetAll();
+        Reservation Get(int id);
+        Reservation Add(Reservation user);
+        Reservation Update(Reservation user);
+        Reservation Delete(int id);
+    }
+}
---
+++ HotelBookingSystem/BookingAPI/Interfaces/IReservationAction.cs
@@ -0,0 +1,15 @@
+﻿using BookingAPI.Models;
+using BookingAPI.Models.DTO;
+
+namespace BookingAPI.Interfaces
+{
+    public interface IReservationAction
+    {
+        Reservation BookReservation(Reservation reservation);
+        Reservation CancelReservation(int id);
+        Reservation EditReservation(Reservation reservation);
+        List<RoomDTO> GetAllBookedRoom(int hotelid);
+        List<Reservation> GetAllReservation();
+
+    }
+}
