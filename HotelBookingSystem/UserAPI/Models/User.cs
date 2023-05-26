ecc1a65c0acd4455cb4bfaf48be8eb859f6dc171
Mathanraj T <madhanmani0601@gmail.com>
2023-05-26 11:44:57 +0530

    Added Models For UserAPI

---
+++ HotelBookingSystem/UserAPI/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+        public byte[]? Password { get; set; }
+        public byte[]? HashKey { get; set; }
+        [Required]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+}
