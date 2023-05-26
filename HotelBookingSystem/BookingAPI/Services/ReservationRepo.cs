1d47661ff93a333a23bbdc57828c3d0123150e52
Mathanraj T <madhanmani0601@gmail.com>
2023-05-26 14:14:05 +0530

    Added Services For BookingAPI

---
+++ HotelBookingSystem/BookingAPI/Services/ReservationRepo.cs
@@ -0,0 +1,64 @@
+﻿using BookingAPI.Interfaces;
+using BookingAPI.Models;
+using System.Diagnostics;
+
+namespace BookingAPI.Services
+{
+    public class ReservationRepo : IReservation
+    {
+        private ReservationContext _context;
+        public ReservationRepo(ReservationContext context ) {
+            _context = context;
+        }
+        public Reservation Add(Reservation reservation)
+        {
+            try
+            {
+                _context.Reservations.Add(reservation);
+                _context.SaveChanges();
+                return reservation;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;    
+            }
+        }
+
+        public Reservation Delete(int id)
+        {
+            var reservation = _context.Reservations.SingleOrDefault( r=>r.Id == id );
+            if( reservation != null )
+            {
+                _context.Reservations.Remove( reservation );
+                _context.SaveChanges();
+            }
+            return reservation;
+        }
+
+        public Reservation Get(int id)
+        {
+            return _context.Reservations.SingleOrDefault(r => r.Id == id);
+        }
+
+        public ICollection<Reservation> GetAll()
+        {
+            return _context.Reservations.ToList();
+        }
+
+        public Reservation Update(Reservation reservation)
+        {
+            var existingreservation = _context.Reservations.SingleOrDefault( r=>r.Id == reservation.Id );
+            if(existingreservation != null )
+            {
+                existingreservation.CheckIn = reservation.CheckIn;
+                existingreservation.CheckOut = reservation.CheckOut;
+                existingreservation.RoomId = reservation.RoomId;
+                existingreservation.HotelId = reservation.HotelId;
+                existingreservation.Username = reservation.Username;
+                _context.SaveChanges();
+            }
+            return existingreservation;
+        }
+    }
+}
---
+++ HotelBookingSystem/BookingAPI/Services/ReservationService.cs
@@ -0,0 +1,75 @@
+﻿using BookingAPI.Interfaces;
+using BookingAPI.Models;
+using BookingAPI.Models.DTO;
+
+namespace BookingAPI.Services
+{
+    public class ReservationService : IReservationAction
+    {
+        private readonly IReservation _resrepo;
+        public ReservationService(IReservation resrepo)
+        {
+            _resrepo = resrepo;
+        }
+        public Reservation BookReservation(Reservation reservation)
+        {
+            var reservations = _resrepo.GetAll();
+            foreach (var item in reservations)
+            {
+                if (item.RoomId == reservation.RoomId && item.HotelId==reservation.HotelId)
+                {
+                    if (reservation.CheckIn >= item.CheckIn && reservation.CheckIn <= item.CheckOut)
+                    {
+                        return null;
+                    }
+                    else if (reservation.CheckOut >= item.CheckIn && reservation.CheckOut <= item.CheckOut)
+                    {
+                        return null;
+                    }
+                    else if (reservation.CheckIn <= item.CheckIn && reservation.CheckOut >= item.CheckOut)
+                    {
+                        return null;
+                    }
+                }
+            }
+            reservation.CheckOut = reservation.CheckOut.Date;
+            reservation.CheckIn = reservation.CheckIn.Date;
+            return _resrepo.Add(reservation);
+
+        }
+
+        public Reservation CancelReservation(Reservation reservation)
+        {
+            return _resrepo.Delete(reservation.Id);
+        }
+
+        public Reservation EditReservation(Reservation reservation)
+        {
+            reservation.CheckOut = reservation.CheckOut.Date;
+            reservation.CheckIn = reservation.CheckIn.Date;
+            return _resrepo.Update(reservation);
+        }
+
+        public List<RoomDTO> GetAllBookedRoom(HotelDTO hotelDTO)
+        {
+            var reservations = _resrepo.GetAll();
+            List<RoomDTO> rooms = new List<RoomDTO>();
+            foreach (var item in reservations)
+            {
+                if (item.HotelId == hotelDTO.Id)
+                {
+                    RoomDTO room = new RoomDTO();
+                    room.Id = item.RoomId;
+                    rooms.Add(room);
+
+                }
+            }
+            return rooms;
+        }
+
+        public List<Reservation> GetAllReservation(UserDTO userDTO)
+        {
+            return _resrepo.GetAll().Where(r=>r.Username == userDTO.Username).ToList();
+        }
+    }
+}
